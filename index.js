3f684f39509ec9662b4c49e6f467bfd18cdb63ac
unknown <pchoi13370@gmail.com>
2019-07-08 00:23:43 -0700

    working out playback function

--- index.js
+++ index.js
@@ -5,24 +5,30 @@ window.addEventListener("load", () => {
   const record = document.querySelector(".record");
   const stop = document.querySelector(".stop");
   const reset = document.querySelector(".reset");
-  console.log(record, stop);
+  const play = document.querySelector(".play");
+  let lastTimeStamp;
   let recordedKeys = [];
   let isRecording = false;
 
   record.addEventListener("click", () => {
+    lastTimeStamp = Date.now();
     isRecording = true;
   });
   stop.addEventListener("click", () => {
     isRecording = false;
   });
   reset.addEventListener("click", () => {
-    isRecording = [];
+    recordedKeys = [];
+  });
+  play.addEventListener("click", function() {
+    console.log(recordedKeys);
+    playRecorded(recordedKeys);
   });
 
   //when transition ends, remove it
   function removeTransition(e) {
     if (e.propertyName !== "background-color") return;
-    e.target.classList.remove("play");
+    e.target.classList.remove("playing");
   }
   //add removetransition eventhandler to all keys
   for (let key of keys) {
@@ -31,18 +37,37 @@ window.addEventListener("load", () => {
 
   function playSound(e) {
     const audio = document.querySelector(`audio[data-key=${e.key}]`);
-    const keys = document.querySelector(`li[data-key=${e.key}]`);
-    //if you're recording, push the key into the array
-    if (isRecording) {
-      recordedKeys.push(keys);
-      console.log(recordedKeys);
-    }
-    console.log(recordedKeys);
+    const key = document.querySelector(`li[data-key=${e.key}]`);
+
     if (!audio) return;
 
     audio.currentTime = 0;
     audio.play();
-    keys.classList.toggle("play");
+    key.classList.toggle("playing");
+    //if recording...
+    if (isRecording) {
+      const keyObject = {
+        dataKey: key.dataset.key,
+        timeStamp: Date.now() - lastTimeStamp
+      };
+      lastTimeStamp = Date.now();
+      recordedKeys.push(keyObject);
+      // console.log(keyObject.timeStamp);
+    }
+  }
+  function playRecorded(arr) {
+    let last = 0;
+    for (let i = 0; i < arr.length; i++) {
+      setTimeout(() => {
+        const audio = document.querySelector(
+          `audio[data-key=${arr[i].dataKey}]`
+        );
+        audio.play();
+        audio.currentTime = 0;
+      }, arr[i].timeStamp + last);
+      console.log(arr[i].timeStamp + last)
+      let = arr[i + 1].timeStamp - arr[i].timeStamp;
+    }
   }
 
   //listen for all keypress events on the window
